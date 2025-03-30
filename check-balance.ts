8e849a6aa06923de31ffc340a0012abac554a3c0
Viacheslav-ua <atph@ukr.net>
2025-03-30 17:32:23 +0000

    fix

--- check-balance.ts
+++ check-balance.ts
@@ -11,7 +11,7 @@ const connection = new Connection(clusterApiUrl("devnet"));
 if (!connection) process.exit(1);
 console.log(`⚡️ Connected to devnet`);
 
-let publicKeyString = process.env["PUBLIC_KEY"] || "9z7uYi1HuhYDsrPoQ8j4PU6XHSht3uiBQD8PjyHxQNrj";
+const publicKeyString = process.env["PUBLIC_KEY"] || "9z7uYi1HuhYDsrPoQ8j4PU6XHSht3uiBQD8PjyHxQNrj";
 
 const publicKey = new PublicKey(publicKeyString);
 const balanceInLamports = await connection.getBalance(publicKey);
