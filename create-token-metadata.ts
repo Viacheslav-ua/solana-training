b3c99a4c6997af9c6b15ed999677455a3c80bf53
Viacheslav-ua <atph@ukr.net>
2025-04-02 16:29:25 +0300

    added create-token-metadata func

---
+++ create-token-metadata.ts
@@ -0,0 +1,74 @@
+import "dotenv/config";
+import {
+  Connection, clusterApiUrl, Keypair, PublicKey, sendAndConfirmTransaction, Transaction,
+} from "@solana/web3.js";
+import { getExplorerLink } from "@solana-developers/helpers";
+import { createCreateMetadataAccountV3Instruction } from "@metaplex-foundation/mpl-token-metadata";
+import bs58 from "bs58";
+
+const privateKey = process.env["PRIVATE_KEY_SLV"];
+if (privateKey === undefined) {
+  console.log("Add PRIVATE_KEY_SLV to .env!");
+  process.exit(1);
+}
+const pk = bs58.decode(privateKey);
+const user = Keypair.fromSecretKey(pk);
+
+const connection = new Connection(clusterApiUrl("devnet"));
+const TOKEN_METADATA_PROGRAM_ID = new PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
+const tokenMintAccount = new PublicKey("AE7AqHZwLw9nvPSpvYFDwwWp68dBsEzQqknWqWguQt1h");
+
+const metadataData = {
+  name: "Solana Bootcamp Training",
+  symbol: "SBT",
+  // Arweave / IPFS / Pinata etc link using metaplex standard for off-chain data
+  uri: "https://arweave.net/1234",
+  sellerFeeBasisPoints: 0,
+  creators: null,
+  collection: null,
+  uses: null,
+};
+
+const [metadataPDA, _metadataBump] = PublicKey.findProgramAddressSync(
+  [
+    Buffer.from("metadata"),
+    TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+    tokenMintAccount.toBuffer(),
+  ],
+  TOKEN_METADATA_PROGRAM_ID
+);
+
+
+const transaction = new Transaction();
+const createMetadataAccountInstruction =
+  createCreateMetadataAccountV3Instruction(
+    {
+      metadata: metadataPDA,
+      mint: tokenMintAccount,
+      mintAuthority: user.publicKey,
+      payer: user.publicKey,
+      updateAuthority: user.publicKey,
+    },
+    {
+      createMetadataAccountArgsV3: {
+        collectionDetails: null,
+        data: metadataData,
+        isMutable: true,
+      },
+    }
+  );
+transaction.add(createMetadataAccountInstruction);
+
+await sendAndConfirmTransaction(
+  connection,
+  transaction,
+  [user]
+);
+
+const tokenMintLink = getExplorerLink(
+  "address",
+  tokenMintAccount.toString(),
+  "devnet"
+);
+console.log(`✅ Look at the token mint again: ${tokenMintLink}`);
+
